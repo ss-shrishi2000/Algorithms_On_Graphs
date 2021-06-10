eb910d47d0d8a114ee596c84257ba2be2b0891ef
Shrishi2000 <65821403+ss-shrishi2000@users.noreply.github.com>
2021-06-11 02:26:24 +0530

    Update strongly_connected.cpp

--- week2_graph_decomposition2/3_intersection_reachability/strongly_connected.cpp
+++ week2_graph_decomposition2/3_intersection_reachability/strongly_connected.cpp
@@ -1,3 +1,20 @@
+Problem Introduction
+The police department of a city has made all streets one-way. You would like
+to check whether it is still possible to drive legally from any intersection to
+any other intersection. For this, you construct a directed graph: vertices are
+intersections, there is an edge (𝑢, 𝑣) whenever there is a (one-way) street from
+𝑢 to 𝑣 in the city. Then, it suffices to check whether all the vertices in the
+graph lie in the same strongly connected component.
+Problem Description
+Task. Compute the number of strongly connected components of a given directed graph with 𝑛 vertices and
+𝑚 edges.
+Input Format. A graph is given in the standard format.
+Constraints. 1 ≤ 𝑛 ≤ 104, 0 ≤ 𝑚 ≤ 104.
+Output Format. Output the number of strongly connected components
+
+
+
+
 #include <algorithm>
 #include <iostream>
 #include <vector>
