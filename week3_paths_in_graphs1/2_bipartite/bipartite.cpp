a5478d3c5e9818645892c269006ca48ef043f87e
Shrishi2000 <65821403+ss-shrishi2000@users.noreply.github.com>
2021-06-21 23:01:58 +0530

    Update bipartite.cpp

--- week3_paths_in_graphs1/2_bipartite/bipartite.cpp
+++ week3_paths_in_graphs1/2_bipartite/bipartite.cpp
@@ -1,3 +1,32 @@
+
+Checking whether a Graph is Bipartite
+Problem Introduction
+An undirected graph is called bipartite if its vertices can be split into two parts such that each edge of the
+graph joins to vertices from different parts. Bipartite graphs arise naturally in applications where a graph
+is used to model connections between objects of two different types (say, boys and girls; or students and
+dormitories).
+An alternative definition is the following: a graph is bipartite if its vertices can be colored with two colors
+(say, black and white) such that the endpoints of each edge have different colors.
+Problem Description
+Task. Given an undirected graph with 𝑛 vertices and 𝑚 edges, check whether it is bipartite.
+Input Format. A graph is given in the standard format.
+Constraints. 1 ≤ 𝑛 ≤ 105, 0 ≤ 𝑚 ≤ 105.
+Output Format. Output 1 if the graph is bipartite and 0 otherwise.
+   
+  Sample 1.
+Input:
+4 4
+1 2
+4 1
+2 3
+3 1
+Output:
+0
+1 2
+4
+
+SOLUTION:-
+
 #include<bits/stdc++.h>
 using namespace std;
 # define vi vector<int>
