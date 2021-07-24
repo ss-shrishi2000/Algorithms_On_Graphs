a4bef7703151152bb446438b957608766e726cf0
Shrishti Singh <65821403+ss-shrishi2000@users.noreply.github.com>
2021-07-25 00:36:07 +0530

    Update shortest_paths.cpp

--- week4_paths_in_graphs2/3_exchanging_money/shortest_paths.cpp
+++ week4_paths_in_graphs2/3_exchanging_money/shortest_paths.cpp
@@ -49,28 +49,34 @@ void shortest_paths(vector<vector<int> > &adj, vector<vector<int> > &cost, int s
 
 int main() {
   int n, m, s;
-  std::cin >> n >> m;
+  cin >> n >> m;
   vector<vector<int> > adj(n, vector<int>());
   vector<vector<int> > cost(n, vector<int>());
-  for (int i = 0; i < m; i++) {
+  for (int i = 0; i < m; i++)
+  {
     int x, y, w;
-    std::cin >> x >> y >> w;
+    cin >> x >> y >> w;
     adj[x - 1].push_back(y - 1);
     cost[x - 1].push_back(w);
   }
-  std::cin >> s;
+  cin >> s;
   s--;
   vector<long long> distance(n, std::numeric_limits<long long>::max());
   vector<int> reachable(n, 0);
   vector<int> shortest(n, 1);
   shortest_paths(adj, cost, s, distance, reachable, shortest);
-  for (int i = 0; i < n; i++) {
-    if (!reachable[i]) {
-      std::cout << "*\n";
-    } else if (!shortest[i]) {
-      std::cout << "-\n";
-    } else {
-      std::cout << distance[i] << "\n";
+  for (int i = 0; i < n; i++)
+  {
+    if (!reachable[i])
+    {
+      cout << "*\n";
+    } 
+   else if (!shortest[i]) 
+   {
+      cout << "-\n";
+    }
+   else {
+      cout << distance[i] << "\n";
     }
   }
 }
